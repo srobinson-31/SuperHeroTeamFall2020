33299e3a8e0c90d12d7c3f8d4b2063842ef9b56d
srobinson-31 <72355963+srobinson-31@users.noreply.github.com>
2020-10-26 23:46:57 -0400

    Revert "Syrone"

--- src/Model/Room.java
+++ src/Model/Room.java
@@ -1,21 +1,5 @@
 package Model;
-import java.util.ArrayList;
 
-public class Room 
-{
-	private String locationID;
-	private char w;
-	private char a;
-	private char d;
-	private String roomDescription;
-	arraylist<Items> Items;
-	private String roomName;
-	private boolean visited;
-	private int[] neighbors;
-	private String puzzle;
-	public String answer;
-	private boolean puzzleAnswer;
-	
-	
-	
+public class Room {
+
 }
